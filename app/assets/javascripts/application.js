659bdcc8a0e245a4b1bb4595f8d88351480d7b74
parameciumm <koq4046@naver.com>
2016-05-28 04:57:27 +0000

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+
+ $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+  
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def weather 
+  end
+  
+  def five_rainy
+  end
+  def five_sunny
+  end
+  def five_cloudy
+  end
+  
+  
+end
